81d2fa937ba823d71935b92d716f692454ce77d2
Juan <juan2ramos@gmail.com>
2020-01-17 17:09:34 -0500

    burndownchart home

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/chart/',
     name: 'chart',
     props: true,
     component: () => import('../views/Chart.vue'),
